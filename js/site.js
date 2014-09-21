aaffc5123c86fb466f86ef129ce104b4baa809d2
Reinier B <ssjkrillen@hotmail.com>
2014-09-21 14:01:14 +0200

    Moved image for follower and subscriber to javascript Change Mu_Anaconda to mp3 format Added not working subscriber w.i.p. code

--- js/site.js
+++ js/site.js
@@ -12,7 +12,7 @@ twitch.pollFollowers();
 
 
 $(document).ready(function() {
-    twitch.setConfig("follower",{callback:popUpFollower,pollTime:2000,loaded:false})
+    twitch.setConfig("follower",{callback:popUpFollower})
     updateAppendFollowers();
 });
 
@@ -22,10 +22,10 @@ function updateAppendFollowers(){
             if(twitch.getRecentFollowers().length !== 0) {
                 $.each(twitch.getRecentFollowers().slice(0,10), function( index, value ) {
                     entry = twitch.getRecentFollowers()[index];
-                    $("#recentFollowers").append( "<li>"+ entry.user.display_name +"</li>" );
+                    $("#recentFollowers").append( '<li>'+ entry.user.display_name +'</li>' );
                 });
             } else {
-                $("#recentFollowers").append( "<li>No followers found</li>" );
+                $("#recentFollowers").append( '<li>No followers found</li>' );
             }
         updateAppendFollowers()
     }, 2000);
@@ -33,22 +33,24 @@ function updateAppendFollowers(){
 
 function popUpFollower(newFollower){
     console.log("New follower pop-up for: " + newFollower.user.display_name)
-    $("#followerName").remove();
-    $("#mostRecentFollowerPopUp").append( "<div id='followerName'>New follower: "+ newFollower.user.display_name +"</div>");
-    twitch.playMusic("mp3/My_Anaconda.wav")
+    $("#mostRecentFollowerPopUp").empty();
+    $("#mostRecentFollowerPopUp").append( '<img id="followerImage" src="img/NewFollower/Dancing_Banana.gif">');
+    $("#mostRecentFollowerPopUp").append( '<div id="followerName">New follower: '+ newFollower.user.display_name +'</div>');
+    twitch.playMusic("mp3/My_Anaconda.mp3")
     $("#mostRecentFollowerPopUp").delay()
         .fadeIn(600)
-        .delay(8000)
+        .delay(7000)
         .fadeOut(600);
 }
 
 function popUpSubscriber(newSubscriber){
     console.log("New subscriber pop-up for: " + newSubscriber.user.display_name)
-    $("#subscriberName").remove();
-    $("#mostRecentSubscriberPopUp").append( "<div id='subscriberName'>New subscriber: "+ newSubscriber.user.display_name +"</div>");
+    $("#mostRecentSubscriberPopUp").empty();
+    $("#mostRecentSubscriberPopUp").append( '<img id="followerImage" src="img/NewSubscriber/blues_clues.gif">');
+    $("#mostRecentSubscriberPopUp").append( '<div id="subscriberName">New subscriber: '+ newSubscriber.user.display_name +'</div>');
     twitch.playMusic("mp3/Youre_Tearing_Me_Apart_Lisa.mp3")
     $("#mostRecentSubscriberPopUp").delay()
         .fadeIn(600)
-        .delay(8000)
+        .delay(4000)
         .fadeOut(600);
 }
